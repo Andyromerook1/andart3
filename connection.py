e65d97c958228e82bd79ed7b12a2262f307582cb
Andyromerook1 <138953718+Andyromerook1@users.noreply.github.com>
2026-08-13 23:31:29 -0300

    connection.py

--- connection.py
+++ connection.py
@@ -1,9 +1,12 @@
 import ssl
 import websockets
-from logger import info, error
+from logger import info, error, success
 
 async def connect_ws(target_uri, headers):
     try:
+        # Falsificar User-Agent si no viene en headers
+        if "User-Agent" not in headers:
+            headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         info(f"Conectando a {target_uri}...")
         ssl_context = ssl.create_default_context()
         connection = await websockets.connect(
